fa7b86ca9257ec1bc119d7af782b9688eab2f3e5
Nguyễn Đức Duy <59301500+ducduy10k@users.noreply.github.com>
2024-09-16 16:03:12 +0700

    Update main.dart
    header

--- lib/main.dart
+++ lib/main.dart
@@ -36,24 +36,76 @@ void main() {
       // ),SubjectBloc
     ],
     child: MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text(
-              'Title aaaaa a   ',
-              // maxLines: 1,
-              // overflow: TextOverflow.ellipsis,
-            ),
-            backgroundColor: Colors.blue,
-            centerTitle: false,
-          ),
-          body: body()),
+      title: "First app by duynd",
+      home: Page(),
     ),
   ));
 }
 
+class Page extends StatelessWidget {
+  const Page({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawer(child: ListView(
+        children: [
+          DrawerHeader(child: Text("Header")),
+          ListTile(
+            title: Text("Menu item"),
+            trailing: Icon(Icons.navigate_next),
+          )
+        ],
+      )),
+        appBar: AppBar(
+          title: Text(
+            'Title app',
+            // maxLines: 1,
+            // overflow: TextOverflow.ellipsis,
+          ),
+          backgroundColor: Colors.blue,
+          centerTitle: false,
+          
+          actions: [
+            IconButton(onPressed: () {}, icon: Icon(
+              Icons.settings, color: Colors.white,
+            )),
+            PopupMenuButton(
+              icon: Icon(Icons.share),
+              color: Colors.white,
+              itemBuilder: (context) {
+                return [
+                  PopupMenuItem(child: Icon(Icons.facebook)),
+                  PopupMenuItem(child: Icon(Icons.mail)),
+                ];
+              },
+            )
+          ],
+          leading: LeadingAppbar(),
+          
+        ),
+        floatingActionButton: FloatingActionButton(onPressed: () {}, child: Icon(Icons.add),),
+        body: body());
+  }
+}
+
+class LeadingAppbar extends StatelessWidget {
+  const LeadingAppbar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(onPressed: () {
+      print("object");
+      Scaffold.of(context).openDrawer();
+    }, icon: Icon(Icons.menu, color: Colors.white,));
+  }
+}
+
 class body extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<CounterBloc, int>(
@@ -61,39 +113,38 @@ class body extends StatelessWidget {
         print(state);
         return Center(
             child: Container(
-              width: double.infinity,
-              height: double.infinity,
-              margin: EdgeInsets.all(20),
-              padding: EdgeInsets.fromLTRB(10, 20, 10, 20),
-              child: BodyContent(),
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                border: Border.all(
-                    color: Colors.grey,
-                    width: 10,
-                    style: BorderStyle.solid,
-                    strokeAlign: BorderSide.strokeAlignInside),
-                color: Colors.green,
-                // shape: BoxShape.circle,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.pink,
-                    offset: Offset(8, 8),
-                    blurRadius: 10,
-                  )
-                ],
+          width: double.infinity,
+          height: double.infinity,
+          margin: EdgeInsets.all(20),
+          padding: EdgeInsets.fromLTRB(10, 20, 10, 20),
+          child: BodyContent(),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            border: Border.all(
+                color: Colors.grey,
+                width: 10,
+                style: BorderStyle.solid,
+                strokeAlign: BorderSide.strokeAlignInside),
+            color: Colors.green,
+            // shape: BoxShape.circle,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.pink,
+                offset: Offset(8, 8),
+                blurRadius: 10,
+              )
+            ],
 
-                // borderRadius: BorderRadius.circular(16)
-              ),
-              // transform: Matrix4.rotationZ(30),
-            ));
+            // borderRadius: BorderRadius.circular(16)
+          ),
+          // transform: Matrix4.rotationZ(30),
+        ));
       },
     );
   }
 }
 
 class BodyItem extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<CounterBloc, int>(
@@ -107,33 +158,23 @@ class BodyItem extends StatelessWidget {
             child: OutlinedButton(
               child: Text("$state"),
               onPressed: () =>
-              {
-                context.read<CounterBloc>().add(new CouterIncrement())
-              },
+                  {context.read<CounterBloc>().add(new CouterIncrement())},
             ),
-
           ),
           decoration: BoxDecoration(
-              color: Colors.blue,
-              borderRadius: BorderRadius.circular(16)
-          ),
-
-
+              color: Colors.blue, borderRadius: BorderRadius.circular(16)),
         );
       },
     );
   }
 }
 
-
 class BodyContent extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Container(
         width: double.infinity,
         height: double.infinity,
-
         child: Column(
           children: [
             Row(
@@ -141,8 +182,49 @@ class BodyContent extends StatelessWidget {
               // crossAxisAlignment: CrossAxisAlignment.stretch,
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               // mainAxisSize: MainAxisSize.min,
-              children: [BodyItem(), BodyItem(), BodyItem(), BodyItem()],
-
+              children: [
+                BodyItem(),
+                BodyItem(),
+                OutlinedButton(
+                    onPressed: () {
+                      showModalBottomSheet(
+                          context: context,
+                          showDragHandle: true,
+                          backgroundColor: Colors.red,
+                          enableDrag: true,
+                          // isScrollControlled: true,
+                          scrollControlDisabledMaxHeightRatio: .9,
+                          // barrierColor: Colors.grey,
+                          builder: (context) {
+                            return Container(
+                              width: double.infinity,
+                              child: const Column(
+                                children: [
+                                  Text("He;;p"),
+                                  Text("He;;p"),
+                                  Text("He;;p"),
+                                  Text("He;;p"),
+                                  Text("He;;p"),
+                                  Text("He;;p"),
+                                  Text("He;;p"),
+                                  Text("He;;p"),
+                                  Text("He;;p"),
+                                ],
+                              ),
+                            );
+                          });
+                    },
+                    child: Text("BtmSheet")),
+                OutlinedButton(
+                    onPressed: () async {
+                      DateTime? date = await showDatePicker(
+                          context: context,
+                          firstDate: DateTime.now(),
+                          lastDate: DateTime.now().add(Duration(days: 14)));
+                      print("Date selected $date");
+                    },
+                    child: Text("Date"))
+              ],
             ),
             Container(
               width: 400,
@@ -163,16 +245,33 @@ class BodyContent extends StatelessWidget {
                     width: 100,
                     height: 100,
                     child: OutlinedButton(
-                      child: Text("+"),
-                      onPressed: () =>
-                      {
-                        context.read<CounterBloc>()
+                      child: Text("Show dialog"),
+                      onPressed: () => {
+                        showDialog(
+                            context: context,
+                            barrierColor: Colors.red,
+                            builder: (context) => AlertDialog(
+                                  title: Text("Title"),
+                                  content: Text("content"),
+                                  actions: [
+                                    TextButton(
+                                        onPressed: () {
+                                          TextButton(
+                                              onPressed: () {},
+                                              child: Text("Cancel"));
+                                        },
+                                        child: Text("Cancel")),
+                                    TextButton(
+                                        onPressed: () {}, child: Text("OK"))
+                                  ],
+                                  alignment: Alignment.topCenter,
+                                  shadowColor: Colors.yellow,
+                                  elevation: 16,
+                                ))
                       },
                     ),
                     decoration: BoxDecoration(
-                        color: Colors.blue,
-                        shape: BoxShape.circle
-                    ),
+                        color: Colors.blue, shape: BoxShape.circle),
                   ),
                   Align(
                     alignment: Alignment.topRight,
@@ -187,9 +286,7 @@ class BodyContent extends StatelessWidget {
                     right: 10,
                     width: 40,
                     height: 40,
-
                     child: Container(
-
                       color: Colors.pink,
                     ),
                   ),
@@ -198,25 +295,25 @@ class BodyContent extends StatelessWidget {
                     left: 10,
                     width: 180,
                     height: 180,
-
                     child: Container(
                       color: Colors.pink,
                       child: Column(
                         children: [
-                          Image.asset('assets/images/tho.jpeg', fit: BoxFit.cover, width: 150,),
+                          Image.asset(
+                            'assets/images/tho.jpeg',
+                            fit: BoxFit.cover,
+                            width: 150,
+                          ),
                           SvgPicture.asset('assets/images/checkin.svg')
                         ],
-                      ) ,
+                      ),
                     ),
                   ),
                 ],
               ),
-            )
+            ),
+
           ],
-
-        )
-
-    );
+        ));
   }
 }
-
